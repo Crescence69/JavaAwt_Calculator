ad2039c7b1daf644b347f7976ef8e964b8cddb83
Cyrus Maquilan <maquilancyrus@gmail.com>
2025-03-25 21:04:57 +0800

    Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,13 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.awtcalculator;
-
-/**
- *
- * @author Almera Perit
- */
 
 import java.awt.*;
 import java.awt.event.*;
